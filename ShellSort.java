c39add423f8bcbabfc604837325b62cad2f76335
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-05-29 22:51:00 +0530

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -6,12 +6,13 @@ public class ShellSort {
         int h = 1;
         while (h < N/3) h = 3*h + 1;
         while (h >= 1) {
-            for (int i=h; i<N; i++)
-                for (int j=i; j>=h && less(a[j], a[j-h]); j-=h) {
-                    exch(a, j, j-h);
+            for (int i=h; i<N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
                 }
+            }
             assert isHsorted(a, h);
-                h /= 3;
+            h /= 3;
         }
         assert isSorted(a);
     }
