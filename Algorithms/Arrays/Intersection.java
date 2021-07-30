a89d61545220533955798d7c86059dd462031846
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-30 19:29:55 +0530

    Update Intersection.java

--- Algorithms/Arrays/Intersection.java
+++ Algorithms/Arrays/Intersection.java
@@ -48,6 +48,54 @@ public class Intersection {
                 if (A.get(i).getX()==B.get(j).getX() && A.get(i).getY()==B.get(j).getY()) count++;
             }
         }
+        
+        System.out.println("A: ");
+        for (int i=0; i<n; i++) {
+            System.out.println(" " + A.get(i).getX() + " " + A.get(i).getY());
+        }
+        
+        System.out.println("B: ");
+        for (int i=0; i<n; i++) {
+            System.out.println(" " + B.get(i).getX() + " " + B.get(i).getY());
+        }
+        
         System.out.println("A intersection B: " + count);
     }
 }
+
+/*      How many points?: 5
+        Input values for Set A and Set B: 
+        A.x1: 0
+        A.y1: 1
+        B.x1: 2
+        B.y1: 3
+        A.x2: 4
+        A.y2: 5
+        B.x2: 6
+        B.y2: 7
+        A.x3: 8
+        A.y3: 9
+        B.x3: 0
+        B.y3: 1
+        A.x4: 2
+        A.y4: 3
+        B.x4: 4
+        B.y4: 5
+        A.x5: 6
+        A.y5: 7
+        B.x5: 8
+        B.y5: 9
+        A: 
+        0 1
+        4 5
+        8 9
+        2 3
+        6 7
+        B: 
+        2 3
+        6 7
+        0 1
+        4 5
+        8 -1
+        A intersection B: 4
+*/
