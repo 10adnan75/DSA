6e1617621cdc25ad108b762b0806c6e377125c84
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-08-02 20:46:27 +0530

    Update ThreeSum.java

--- Algorithms/Arrays/ThreeSum.java
+++ Algorithms/Arrays/ThreeSum.java
@@ -4,15 +4,14 @@ import java.io.*;
 
 public class ThreeSum {
     
-    public static void threeSum(int[] a) {
-        Arrays.sort(a);
+    public static void threeSum(int[] a, int key) {
         for (int i=0; i<a.length-2; i++) {
             int j = i+1;
             int k = a.length-1;
             while (j < k) {
                 int sum = a[i] + a[j] + a[k];
-                if (sum == 0) System.out.println(a[i] + " + " + a[j] + " + " + a[k] + " = " + sum);
-                if (sum >= 0) {
+                if (sum == key) System.out.println(a[i] + " + " + a[j] + " + " + a[k] + " = " + sum);
+                if (sum >= key) {
                     k--;
                 } else {
                     j++;
@@ -26,15 +25,16 @@ public class ThreeSum {
         System.out.print("Enter size: "); int size = input.nextInt();
         int[] a = new int[size];
         System.out.print("Enter keys: "); for (int i=0; i<size; i++) a[i] = input.nextInt();
+        Arrays.sort(a);
         System.out.print("Solution:\n");
-        threeSum(a);
+        for (int i=0; i<size; i++) threeSum(a, a[i]);
     }
 }
 
-/*      OUTPUT:
-        Enter size: 5
-        Enter keys: 1 2 -1 0 -2
+
+/*      Enter size: 5
+        Enter keys: 11 2 1 6 3
         Solution:
-        -2 + 0 + 2 = 0
-        -1 + 0 + 1 = 0
+        1 + 2 + 3 = 6
+        2 + 3 + 6 = 11
 */
